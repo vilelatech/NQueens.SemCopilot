37c2591d8c80f3c352cf93b6b6ade81ac60521f0
Bruno Vilela <bruno@vilelatech.com>
2023-09-16 11:11:25 -0300

    NQueens Solved

---
+++ NQueens.SemCopilot/NQueens.SemCopilot/NQueensProblem.cs
@@ -0,0 +1,84 @@
+﻿namespace NQueens.SemCopilot;
+
+public class NQueensProblem
+{
+    private int size = 8;
+    private int[,] chessboard;
+
+    public NQueensProblem(int size)
+    {
+        this.size = size;
+        this.chessboard = new int[size, size];
+        SolveNQueens(chessboard, 0);
+        Print(this.chessboard);
+    }
+
+
+    private bool SolveNQueens(int[,] board, int col)
+    {
+        if (col >= size)
+        {
+            return true;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            if (IsSafe(board, i, col))
+            {
+                board[i, col] = 1;
+                if (SolveNQueens(board, col + 1))
+                {
+                    return true;
+                }
+
+                board[i, col] = 0;
+            }
+        }
+
+        return false;
+    }
+
+    private bool IsSafe(int[,] board, int row, int col)
+    {
+        int i, j;
+
+        for (i = 0; i < col; i++)
+        {
+            if (board[row, i] == 1)
+            {
+                return false;
+            }
+        }
+
+        for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+        {
+            if (board[i, j] == 1)
+            {
+                return false;
+            }
+        }
+
+        for (i = row, j = col; j >= 0 && i < size; i++, j--)
+        {
+            if (board[i, j] == 1)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void Print(int[,] board)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write(board[i, j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
--- NQueens.SemCopilot/NQueens.SemCopilot/Program.cs
+++ NQueens.SemCopilot/NQueens.SemCopilot/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using NQueens.SemCopilot;
 
-Console.WriteLine("Hello, World!");
+NQueensProblem nQueensProblem = new NQueensProblem(16);
